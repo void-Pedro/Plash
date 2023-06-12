5de539d0211aa8c612b17289e0d12a98bcd8b5c3
Pedro <pedro.borges@estudante.ufscar.br>
2023-06-11 21:58:13 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+    Implementação de um Shell simplificado para a disciplina de Sistemas Operacionais
+    Autores: Pedro Henrique Borges - 804071
+             Luís Fernando do Carmo - 
+
+    Casos aceitos no Shell:
+        a. $ prog - Processo em foreground
+        b. $ prog & - Processo em background
+        c. $ prog parâmetros - Processo com parâmetros (separados por espaços ou vetor de strings)
+        d. $ prog parâmetros & - Processo com parâmetros em bg
+        e. $ prog [parâmetros] > arquivo - Processo com parâmetros que envia o output para um arquivo
+        f. $ prog1 | prog 2 - Processo que envia sua saída para outro processo através de um pipe
+
+*/
